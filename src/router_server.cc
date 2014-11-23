c885013f0fa06e43770ce0093bbdf8d2afeedb6e
wyy <wuyanyi09@gmail.com>
2014-11-23 17:44:59 +0800

    ci

--- src/router_server.cc
+++ src/router_server.cc
@@ -1,118 +1,111 @@
 #include "router_server.h"
 
 DEFINE_int32(listen_port, 8080, "");
+DEFINE_int32(read_buffer_size,  16483, "");
 
 namespace xcomet {
 
-RouterServer::RouterServer() {
+RouterServer::RouterServer() { 
+  Init();
 }
 
 RouterServer::~RouterServer() {
 }
 
+void RouterServer::Init() {
+  evbase_ = event_base_new();
+  CHECK(evbase_);
+
+  struct sockaddr_in sin;
+  sin.sin_family = AF_INET;
+  sin.sin_addr.s_addr = 0;
+  sin.sin_port = htons(FLAGS_listen_port);
+
+  listener_ = socket(AF_INET, SOCK_STREAM, 0);
+  evutil_make_socket_nonblocking(listener_);
+
+  CHECK(-1 != bind(listener_, (struct sockaddr*)&sin, sizeof(sin))) << strerror(errno); 
+
+  CHECK(-1 != listen(listener_, 16)) << strerror(errno);
+
+
+  listener_event_ = event_new(evbase_, listener_, EV_READ|EV_PERSIST, AcceptCB, static_cast<void*>(this));
+  CHECK(-1 != event_add(listener_event_, NULL));
+
+  LOG(INFO) << "start server, port:" << FLAGS_listen_port;
+}
+
 void RouterServer::Start() {
-    evutil_socket_t listener;
-    struct sockaddr_in sin;
-    struct event_base *base;
-    struct event *listener_event;
 
-    base = event_base_new();
-    if (!base)
-        return; /*XXXerr*/
-
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = 0;
-    sin.sin_port = htons(FLAGS_listen_port);
-
-    listener = socket(AF_INET, SOCK_STREAM, 0);
-    evutil_make_socket_nonblocking(listener);
-
-    if (bind(listener, (struct sockaddr*)&sin, sizeof(sin)) < 0) {
-        perror("bind");
-        return;
-    }
-
-    if (listen(listener, 16)<0) {
-        perror("listen");
-        return;
-    }
-
-    listener_event = event_new(base, listener, EV_READ|EV_PERSIST, AcceptCB, (void*)base);
-    /*XXX check it */
-    event_add(listener_event, NULL);
-
-    LOG(INFO) << "start server, port:" << FLAGS_listen_port;
-    event_base_dispatch(base);
+  event_base_dispatch(evbase_);
 }
 
 void RouterServer::AcceptCB(evutil_socket_t listener, short event, void *arg) {
-    struct event_base *base = (struct event_base*)arg;
-    struct sockaddr_storage ss;
-    socklen_t slen = sizeof(ss);
-    int fd = accept(listener, (struct sockaddr*)&ss, &slen);
-    if (fd < 0) {
-        perror("accept");
-    } else if (fd > FD_SETSIZE) {
-        close(fd);
-    } else {
-        struct bufferevent *bev;
-        evutil_make_socket_nonblocking(fd);
-        bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
-        bufferevent_setcb(bev, ReadCB, NULL, ErrorCB, NULL);
-        bufferevent_setwatermark(bev, EV_READ, 0, MAX_LINE);
-        bufferevent_enable(bev, EV_READ|EV_WRITE);
-    }
+  VLOG(5) << size_t(arg);
+  //struct event_base *base = (struct event_base*)arg;
+  RouterServer* that = static_cast<RouterServer*>(arg);
+  CHECK(that);
+  struct event_base *base = that->evbase_;
+  CHECK(base);
+  struct sockaddr_storage ss;
+  socklen_t slen = sizeof(ss);
+  int fd = accept(listener, (struct sockaddr*)&ss, &slen);
+  if (fd < 0) {
+    LOG(ERROR) << strerror(errno);
+  } else if (fd > FD_SETSIZE) {
+    LOG(ERROR) << "accept socket fd > FD_SETSIZE, close it.";
+    close(fd);
+  } else {
+    VLOG(5) << "new a socket bufferevent";
+    struct bufferevent *bev;
+    evutil_make_socket_nonblocking(fd);
+    bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
+    bufferevent_setcb(bev, ReadCB, NULL, ErrorCB, NULL);
+    bufferevent_setwatermark(bev, EV_READ, 0, FLAGS_read_buffer_size);
+    bufferevent_enable(bev, EV_READ|EV_WRITE);
+  }
 }
 
 void RouterServer::ReadCB(struct bufferevent* bev, void *ctx) {
-    struct evbuffer *input, *output;
-    char *line;
-    size_t n;
-    int i;
-    input = bufferevent_get_input(bev);
-    output = bufferevent_get_output(bev);
+  VLOG(5) << "enter ReadCB";
+  struct evbuffer *input, *output;
+  char buf[FLAGS_read_buffer_size];
+  input = bufferevent_get_input(bev);
+  output = bufferevent_get_output(bev);
 
-    while ((line = evbuffer_readln(input, &n, EVBUFFER_EOL_LF))) {
-        for (i = 0; i < n; ++i)
-            line[i] = rot13_char(line[i]);
-        evbuffer_add(output, line, n);
-        evbuffer_add(output, "\n", 1);
-        free(line);
-    }
-
-    if (evbuffer_get_length(input) >= MAX_LINE) {
-        /* Too long; just process what there is and go on so that the buffer
-         * doesn't grow infinitely long. */
-        char buf[1024];
-        while (evbuffer_get_length(input)) {
-            int n = evbuffer_remove(input, buf, sizeof(buf));
-            for (i = 0; i < n; ++i)
-                buf[i] = rot13_char(buf[i]);
-            evbuffer_add(output, buf, n);
-        }
-        evbuffer_add(output, "\n", 1);
-    }
+  while (evbuffer_get_length(input)) {
+    int n = evbuffer_remove(input, buf, sizeof(buf));
+    VLOG(5) << "evbuffer_remove";
+    evbuffer_add(output, buf, n);
+  }
+  // TODO bug when input's length equal to FLAGS_read_buffer_size
+  evbuffer_add(output, "\n", 1);
+  VLOG(5) << "finished ReadCB";
 }
 
 void RouterServer::ErrorCB(struct bufferevent* bev, short error, void *ctx) {
-    if (error & BEV_EVENT_EOF) {
-        /* connection has been closed, do any clean up here */
-        /* ... */
-    } else if (error & BEV_EVENT_ERROR) {
-        /* check errno to see what error occurred */
-        /* ... */
-    } else if (error & BEV_EVENT_TIMEOUT) {
-        /* must be a timeout event handle, handle it */
-        /* ... */
-    }
-    bufferevent_free(bev);
+  if (error & BEV_EVENT_EOF) {
+    /* connection has been closed, do any clean up here */
+    /* ... */
+    VLOG(5) << "connection has been closed from remote.";
+  } else if (error & BEV_EVENT_ERROR) {
+    /* check errno to see what error occurred */
+    /* ... */
+    LOG(ERROR) << strerror(EVUTIL_SOCKET_ERROR());
+  } else if (error & BEV_EVENT_TIMEOUT) {
+    /* must be a timeout event handle, handle it */
+    /* ... */
+    VLOG(5) << "timeout happened";
+  }
+  bufferevent_free(bev);
 }
 
 } // namespace xcomet
 
 int main(int argc, char ** argv) {
-    xcomet::RouterServer server;
-    server.Start();
-    return EXIT_SUCCESS;
+  base::ParseCommandLineFlags(&argc, &argv, false);
+  xcomet::RouterServer server;
+  server.Start();
+  return EXIT_SUCCESS;
 }
 
--- src/router_server.h
+++ src/router_server.h
@@ -19,23 +19,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <map>
 
 #include <iostream>
-
-
-const size_t MAX_LINE = 16384;
-
-
-char rot13_char(char c) {
-    /* We don't want to use isalpha here; setting the locale would change
-     * which characters are considered alphabetical. */
-    if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
-        return c + 13;
-    else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
-        return c - 13;
-    else
-        return c;
-}
 
 using namespace std;
 
@@ -47,10 +33,18 @@ class RouterServer {
         ~RouterServer();
     public:
         void Start();
-    public: // callbacks
+    private: // callbacks
         static void AcceptCB(evutil_socket_t listener, short event, void *arg);
         static void ReadCB(struct bufferevent* bev, void *ctx);
         static void ErrorCB(struct bufferevent* bev, short error, void *ctx);
+    private:
+        void Init();
+    private:
+        //map<>
+    private: // socket and libevent
+        evutil_socket_t listener_;
+        struct event_base *evbase_;
+        struct event *listener_event_;
 };
 
 } // namespace xcomet
