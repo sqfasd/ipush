deeb0fbc58693d9497ef6259ee4bc7642f688cd6
sqfasd <sqf1225@foxmail.com>
2014-11-23 16:06:39 +0800

    move session_server implemention from head to source file

--- src/session_server.cc
+++ src/session_server.cc
@@ -1,5 +1,6 @@
 #include "src/session_server.h"
 
+#include <unistd.h>
 #include <signal.h>
 #include <evhttp.h>
 #include <event2/event.h>
@@ -12,6 +13,153 @@ DEFINE_int32(client_listen_port, 9000, "");
 DEFINE_int32(admin_listen_port, 9100, "");
 DEFINE_int32(user_timeout_sec, 600, "");
 DEFINE_int32(timer_interval_sec, 1, "");
+
+namespace xcomet {
+
+SessionServer::SessionServer()
+    : storage_(router_),
+      timeout_queue_(FLAGS_user_timeout_sec / FLAGS_timer_interval_sec) {
+}
+
+SessionServer::~SessionServer() {
+}
+
+void SessionServer::Sub(struct evhttp_request* req) {
+  if(evhttp_request_get_command(req) != EVHTTP_REQ_GET) {
+    // TODO ReplyError module
+    evhttp_send_reply(req, 405, "Method Not Allowed", NULL);
+    return;
+  }
+
+  HttpQuery query(req);
+  string uid = query.GetStr("uid", "");
+  if (uid.empty()) {
+    evhttp_send_reply(req, 410, "Invalid parameters", NULL);
+    return;
+  }
+
+  int type = query.GetInt("type", 1);
+  // string token = query.GetStr("token", "");
+  // TODO check request parameters
+
+  UserPtr user;
+  UserMap::iterator iter = users_.find(uid);
+  if (iter != users_.end()) {
+    user = iter->second;
+    user->SetType(type);
+    LOG(INFO) << "user has already connected: " << uid;
+    evhttp_send_reply(req, 406, "The user has already connected", NULL);
+    return;
+  } else {
+    user.reset(new User(uid, type, req, *this));
+    users_[uid] = user;
+    router_.RegisterUser(uid);
+  }
+  timeout_queue_.PushUserBack(user.get());
+
+  user->Start();
+  if (storage_.HasOfflineMessage(uid)) {
+    MessageIterator iter = storage_.GetOfflineMessageIterator(uid);
+    while (iter.HasNext()) {
+      user->Send(iter.Next());
+    }
+    storage_.RemoveOfflineMessages(uid);
+  }
+}
+
+// /pub?uid=123&content=hello
+// /pub?cid=123&content=hello
+void SessionServer::Pub(struct evhttp_request* req) {
+  if(evhttp_request_get_command(req) != EVHTTP_REQ_GET) {
+    // TODO ReplyError module
+    evhttp_send_reply(req, 405, "Method Not Allowed", NULL);
+    return;
+  }
+
+  HttpQuery query(req);
+  string content = query.GetStr("content", "");
+  string uid = query.GetStr("uid", "");
+
+  if (!uid.empty()) {
+    LOG(INFO) << "pub to user: " << content;
+    UserMap::iterator iter = users_.find(uid);
+    if (iter != users_.end()) {
+      UserPtr user = iter->second;
+      LOG(INFO) << "send to user: " << user->GetUid();
+      user->Send(content);
+      timeout_queue_.PushUserBack(user.get());
+    } else {
+      router_.Redirect(req);
+    }
+  } else {
+    string cid = query.GetStr("cid", "");
+    if (cid.empty()) {
+      LOG(INFO) << "uid and cid both empty";
+      evhttp_send_reply(req, 410, "Invalid parameters", NULL);
+      return;
+    }
+    LOG(INFO) << "pub to channel: " << content;
+    ChannelMap::iterator iter = channels_.find(cid);
+    if (iter != channels_.end()) {
+      iter->second->Broadcast(content);
+    } else {
+      router_.Redirect(req);
+    }
+  }
+  ReplyOK(req);
+}
+
+// /unsub?uid=123&resource=abc
+void SessionServer::Unsub(struct evhttp_request* req) {
+}
+
+// /createroom?uid=123
+// return roomid
+void SessionServer::CreateRoom(struct evhttp_request* req) {
+}
+
+void SessionServer::DestroyRoom(struct evhttp_request* req) {
+}
+
+// /broadcast?content=hello
+void SessionServer::Broadcast(struct evhttp_request* req) {
+}
+
+// /join?cid=123&uid=456&resource=work
+void SessionServer::Join(struct evhttp_request* req) {
+}
+
+// /leave?cid=123&uid=456&resource=work
+void SessionServer::Leave(struct evhttp_request* req) {
+}
+
+void SessionServer::OnTimer() {
+  VLOG(3) << "OnTimer";
+  DLinkedList<User*> timeout_users = timeout_queue_.PopFront();
+  DLinkedList<User*>::Iterator it = timeout_users.GetIterator();
+  while (User* user = it.Next()) {
+    user->Close();     
+  }
+  timeout_queue_.IncHead();
+}
+
+void SessionServer::RemoveUser(User* user) {
+  const string& uid = user->GetUid();
+  LOG(INFO) << "RemoveUser: " << uid;
+  timeout_queue_.RemoveUser(user);
+  router_.UnregisterUser(uid);
+  users_.erase(uid);
+}
+
+void SessionServer::ReplyOK(struct evhttp_request* req) {
+  evhttp_add_header(req->output_headers, "Content-Type", "text/javascript; charset=utf-8");
+  struct evbuffer *buf = evbuffer_new();
+  evbuffer_add_printf(buf, "{\"type\":\"ok\"}\n");
+  evhttp_send_reply(req, 200, "OK", buf);
+  evbuffer_free(buf);
+}
+
+}  // namespace xcomet
 
 using xcomet::SessionServer;
 
--- src/session_server.h
+++ src/session_server.h
@@ -10,9 +10,6 @@
 #include "src/router_proxy.h"
 #include "src/http_query.h"
 
-DECLARE_int32(user_timeout_sec);
-DECLARE_int32(timer_interval_sec);
-
 namespace xcomet {
 
 class SessionServer {
@@ -24,146 +21,22 @@ class SessionServer {
     return *Singleton<SessionServer>::get();
   }
 
-  // sub?uid=123&token=abc&type=1[&timeout=30][&noroute=true]
-  void Sub(struct evhttp_request* req) {
-    if(evhttp_request_get_command(req) != EVHTTP_REQ_GET) {
-      // TODO ReplyError module
-      evhttp_send_reply(req, 405, "Method Not Allowed", NULL);
-      return;
-    }
+  void Sub(struct evhttp_request* req);
+  void Pub(struct evhttp_request* req);
+  void Unsub(struct evhttp_request* req);
+  void CreateRoom(struct evhttp_request* req);
+  void DestroyRoom(struct evhttp_request* req);
+  void Broadcast(struct evhttp_request* req);
+  void Join(struct evhttp_request* req);
+  void Leave(struct evhttp_request* req);
+  void OnTimer();
 
-    HttpQuery query(req);
-    string uid = query.GetStr("uid", "");
-    if (uid.empty()) {
-      evhttp_send_reply(req, 410, "Invalid parameters", NULL);
-      return;
-    }
+  void RemoveUser(User* user);
 
-    int type = query.GetInt("type", 1);
-    // string token = query.GetStr("token", "");
-    // TODO check request parameters
-
-    UserPtr user;
-    UserMap::iterator iter = users_.find(uid);
-    if (iter != users_.end()) {
-      user = iter->second;
-      user->SetType(type);
-      LOG(INFO) << "user has already connected: " << uid;
-      evhttp_send_reply(req, 406, "The user has already connected", NULL);
-      return;
-    } else {
-      user.reset(new User(uid, type, req, *this));
-      users_[uid] = user;
-      router_.RegisterUser(uid);
-    }
-    timeout_queue_.PushUserBack(user.get());
-
-    user->Start();
-    if (storage_.HasOfflineMessage(uid)) {
-      MessageIterator iter = storage_.GetOfflineMessageIterator(uid);
-      while (iter.HasNext()) {
-        user->Send(iter.Next());
-      }
-      storage_.RemoveOfflineMessages(uid);
-    }
-  }
-
-  // /pub?uid=123&content=hello
-  // /pub?cid=123&content=hello
-  void Pub(struct evhttp_request* req) {
-    if(evhttp_request_get_command(req) != EVHTTP_REQ_GET) {
-      // TODO ReplyError module
-      evhttp_send_reply(req, 405, "Method Not Allowed", NULL);
-      return;
-    }
-
-    HttpQuery query(req);
-    string content = query.GetStr("content", "");
-    string uid = query.GetStr("uid", "");
-
-    if (!uid.empty()) {
-      LOG(INFO) << "pub to user: " << content;
-      UserMap::iterator iter = users_.find(uid);
-      if (iter != users_.end()) {
-        UserPtr user = iter->second;
-        LOG(INFO) << "send to user: " << user->GetUid();
-        user->Send(content);
-        timeout_queue_.PushUserBack(user.get());
-      } else {
-        router_.Redirect(req);
-      }
-    } else {
-      string cid = query.GetStr("cid", "");
-      if (cid.empty()) {
-        LOG(INFO) << "uid and cid both empty";
-        evhttp_send_reply(req, 410, "Invalid parameters", NULL);
-        return;
-      }
-      LOG(INFO) << "pub to channel: " << content;
-      ChannelMap::iterator iter = channels_.find(cid);
-      if (iter != channels_.end()) {
-        iter->second->Broadcast(content);
-      } else {
-        router_.Redirect(req);
-      }
-    }
-    ReplyOK(req);
-  }
-
-  // /unsub?uid=123&resource=abc
-  void Unsub(struct evhttp_request* req) {
-  }
-
-  // /createroom?uid=123
-  // return roomid
-  void CreateRoom(struct evhttp_request* req) {
-  }
-
-  void DestroyRoom(struct evhttp_request* req) {
-  }
-
-  // /broadcast?content=hello
-  void Broadcast(struct evhttp_request* req) {
-  }
-
-  // /join?cid=123&uid=456&resource=work
-  void Join(struct evhttp_request* req) {
-  }
-
-  // /leave?cid=123&uid=456&resource=work
-  void Leave(struct evhttp_request* req) {
-  }
-
-  void OnTimer() {
-    VLOG(3) << "OnTimer";
-    DLinkedList<User*> timeout_users = timeout_queue_.PopFront();
-    DLinkedList<User*>::Iterator it = timeout_users.GetIterator();
-    while (User* user = it.Next()) {
-      user->Close();     
-    }
-    timeout_queue_.IncHead();
-  }
-
-  void RemoveUser(User* user) {
-    const string& uid = user->GetUid();
-    LOG(INFO) << "RemoveUser: " << uid;
-    timeout_queue_.RemoveUser(user);
-    router_.UnregisterUser(uid);
-    users_.erase(uid);
-  }
  private:
-  SessionServer()
-    : storage_(router_),
-      timeout_queue_(FLAGS_user_timeout_sec / FLAGS_timer_interval_sec) {
-  }
-  ~SessionServer() {}
-  void ReplyOK(struct evhttp_request* req) {
-    evhttp_add_header(req->output_headers, "Content-Type", "text/javascript; charset=utf-8");
-    struct evbuffer *buf = evbuffer_new();
-    evbuffer_add_printf(buf, "{\"type\":\"ok\"}\n");
-    evhttp_send_reply(req, 200, "OK", buf);
-    evbuffer_free(buf);
-  }
+  SessionServer();
+  ~SessionServer();
+  void ReplyOK(struct evhttp_request* req);
 
   UserMap users_;
   ChannelMap channels_;
