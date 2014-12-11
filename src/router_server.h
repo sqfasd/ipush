080ac5beb00bed39a0e3db433b388c3be38da0ad
wyy <wuyanyi09@gmail.com>
2014-12-11 16:20:32 +0800

    class private style

--- src/router_server.h
+++ src/router_server.h
@@ -45,7 +45,7 @@ class RouterServer {
  public:
   RouterServer();
   ~RouterServer();
- public:
+
   void Start();
   void MakePubEvent(const char* uid, const char* data, size_t len);
   void ChunkedMsgHandler(size_t sid, const char* buffer, size_t len);
@@ -53,45 +53,38 @@ class RouterServer {
   void ResetSubClient(size_t id);
   void InitSubCliAddrs();
   void InitSubClients();
- private:
+
   void InitPubCliAddrs();
   void InitPubClients();
- private:
   void InitStorage(const string& path);
- private:
-  map<UserID, SessionServerID> u2sMap_;
- private:
+  
   SessionServerID FindServerIdByUid(const UserID& uid) const;
   void InsertUid(const UserID& uid, SessionServerID sid);
   void EraseUid(const UserID& uid) ;
- private:
-  vector<string> subcliaddrs_;
-  vector<string> pubcliaddrs_;
-  vector<shared_ptr<SessionSubClient> > session_sub_clients_;
-  vector<shared_ptr<SessionPubClient> > session_pub_clients_;
- private: //callbacks
+
   static void ClientErrorCB(int sock, short which, void *arg);
- private:
   void PushMsgDoneCB(bool ok);
-  //void PopMsgDoneCB(UserID uid, MessageIteratorPtr mit);
   void GetMsgToPubCB(UserID uid, int64_t start, MessageIteratorPtr mit);
   void GetMsgToReplyCB(UserID uid, struct evhttp_request * req, MessageIteratorPtr mit);
   void GetMsgToPub(const UserID& uid, int64_t start);
- private: 
-  struct event_base *evbase_;
-  scoped_ptr<Storage> storage_;
- private:
   void InitAdminHttp();
-  struct evhttp* admin_http_;
   static void AdminPubCB(struct evhttp_request* req, void *ctx);
   static void AdminBroadcastCB(struct evhttp_request* req, void *ctx);
   static void AdminCheckPresenceCB(struct evhttp_request* req, void * ctx);
   static void AdminCheckOffMsgCB(struct evhttp_request* req, void *ctx);
   static void AcceptErrorCB(struct evconnlistener * listener , void *ctx);
   static void SendReply(struct evhttp_request* req,  const char* content, size_t len);
- private:
   void ReplyError(struct evhttp_request* req);
   void ReplyOK(struct evhttp_request* req);
+
+  map<UserID, SessionServerID> u2sMap_;
+  vector<string> subcliaddrs_;
+  vector<string> pubcliaddrs_;
+  vector<shared_ptr<SessionSubClient> > session_sub_clients_;
+  vector<shared_ptr<SessionPubClient> > session_pub_clients_;
+  struct event_base *evbase_;
+  scoped_ptr<Storage> storage_;
+  struct evhttp* admin_http_;
 };
 
 } // namespace xcomet
