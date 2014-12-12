69a05a7981b0c59550693b17b988507439fb16a1
sqfasd <sqf1225@foxmail.com>
2014-12-12 16:08:48 +0800

    fix channel broadcast issue

--- src/channel.cc
+++ src/channel.cc
@@ -1,5 +1,36 @@
 #include "src/channel.h"
+#include "deps/base/logging.h"
 
 namespace xcomet {
+
+Channel::Channel(const string& id) : id_(id) {
+  CHECK(!id_.empty());
+  VLOG(3) << "Channel construct: " << id_;
+}
+
+Channel::~Channel() {
+  VLOG(3) << "Channel destroy: " << id_;
+}
+
+void Channel::AddUser(User* user) {
+  VLOG(3) << "channel AddUser: " << id_ << ", " << user->GetId();
+  users_.insert(make_pair(user->GetId(), user));
+  user->SetChannelId(id_);
+}
+
+void Channel::RemoveUser(User* user) {
+  VLOG(3) << "channel RemoveUser: " << id_ << ", " << user->GetId();
+  users_.erase(user->GetId());
+  user->SetChannelId("-1");
+}
+
+void Channel::Broadcast(const string& content) {
+  map<string, User*>::iterator it = users_.begin();
+  while (it != users_.end()) {
+    VLOG(3) << "channel Broadcast to: " << it->second->GetId();
+    it->second->Send2(content);
+    ++it;
+  }
+}
 
 }  // namespace xcomet
--- src/channel.h
+++ src/channel.h
@@ -1,8 +1,6 @@
 #ifndef SRC_CHANNEL_H_
 #define SRC_CHANNEL_H_
 
-#include "deps/base/dlist.h"
-#include "deps/base/logging.h"
 #include "deps/base/shared_ptr.h"
 #include "src/include_std.h"
 #include "src/user.h"
@@ -16,39 +14,17 @@ typedef map<string, ChannelPtr> ChannelMap;
 
 class Channel {
  public:
-  Channel(const string& id) : id_(id) {
-    CHECK(!id_.empty());
-    VLOG(3) << "Channel construct: " << id_;
-  }
-  ~Channel() {
-    VLOG(3) << "Channel destroy: " << id_;
-  }
+  Channel(const string& id);
+  ~Channel();
   string GetId() const {return id_;}
-  int GetUserCount() const {return users_.Size();}
-  void AddUser(User* user) {
-    VLOG(3) << "channel AddUser: " << id_ << ", " << user->GetId();
-    users_.PushBack(user);
-    user->SetChannelId(id_);
-    VLOG(3) << "channel size: " << users_.Size();
-  }
-
-  void RemoveUser(User* user) {
-    VLOG(3) << "channel RemoveUser: " << id_ << ", " << user->GetId();
-    users_.Remove(user);
-    user->SetChannelId("-1");
-  }
-  void Broadcast(const string& content) {
-    VLOG(3) << "channel size: " << users_.Size(); 
-    DLinkedList<User*>::Iterator it = users_.GetIterator();
-    while (User* user = it.Next()) {
-      VLOG(3) << "channel Broadcast to: " << user->GetId();
-      user->Send2(content);
-    }
-  }
+  int GetUserCount() const {return users_.size();}
+  void AddUser(User* user);
+  void RemoveUser(User* user);
+  void Broadcast(const string& content);
 
  private:
   string id_;
-  base::DLinkedList<User*> users_;
+  map<string, User*> users_;
 };
 }  // namespace xcomet
 #endif  // SRC_CHANNEL_H_
