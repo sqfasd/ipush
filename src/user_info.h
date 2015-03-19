48981d39e94d1c7050707f41e3cd9e6ad5366cea
sqfasd <sqf1225@foxmail.com>
2015-03-19 15:13:55 +0800

    add user_info

---
+++ src/user_info.h
@@ -0,0 +1,20 @@
+#ifndef SRC_USER_INFO_H_
+#define SRC_USER_INFO_H_
+
+#include "src/include_std.h"
+
+class UserInfo {
+ public:
+  UserInfo(const string& uid) :uid_(uid) {}
+  ~UserInfo() {}
+  string GetId() const {return uid_;}
+  const set<string>& GetChannelIds() const {return channel_ids_;}
+  void SubChannel(const string& cid) {channel_ids_.insert(cid);}
+  void UnsubChannel(const string& cid) {channel_ids_.erase(cid);}
+  string GetId() const {return uid_;}
+
+ private:
+  string uid_;
+  set<string> channel_ids_;
+};
+#endif  // SRC_USER_INFO_H_
