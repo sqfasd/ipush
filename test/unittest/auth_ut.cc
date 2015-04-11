5091e7303ef398b9018e4bcc47e71e8c9f666f52
sqfasd <sqf1225@foxmail.com>
2015-04-11 16:46:32 +0800

    add test case for auth unittest

--- test/unittest/auth_ut.cc
+++ test/unittest/auth_ut.cc
@@ -6,16 +6,27 @@
 
 namespace xcomet {
 
-const char* const TEST_USER = "user";
-const char* const TEST_PWD = "dTxBq4Zkuj/amoApyrVmb6HZP7xPRo9Qi8FQKbAS4umQWMEJjy1Ii7JfXOW4WYoZLUatQ8lY3HeqYRyRcjlUZWJLuiAxEbJAp1zT4bTeM8aJSNm46NkapyMMn1LK9FlgDspChcGCaXaViyx8WLbm9a7gPtzngk4mfRRj/osjpgd++sRXggAybawKl4N+XC3iFSY3JQ/R4YpK02TIG5ydrZDdJOxLzNcH23jyBwLDdDwATZM/tJ2w+fxbHpCCnhxkro0AE2nzWQNGAStGv5jWcoabmGqldR2Ia+5cmPexnHI5OBfdSeohYUNskv+aCrIIVbyIXXNrzJGggSH5P3QVOw==";
+const char* const TEST_USER1 = "user1";
+const char* const TEST_PWD1 = "dTxBq4Zkuj/amoApyrVmb6HZP7xPRo9Qi8FQKbAS4umQWMEJjy1Ii7JfXOW4WYoZLUatQ8lY3HeqYRyRcjlUZWJLuiAxEbJAp1zT4bTeM8aJSNm46NkapyMMn1LK9FlgDspChcGCaXaViyx8WLbm9a7gPtzngk4mfRRj/osjpgd++sRXggAybawKl4N+XC3iFSY3JQ/R4YpK02TIG5ydrZDdJOxLzNcH23jyBwLDdDwATZM/tJ2w+fxbHpCCnhxkro0AE2nzWQNGAStGv5jWcoabmGqldR2Ia+5cmPexnHI5OBfdSeohYUNskv+aCrIIVbyIXXNrzJGggSH5P3QVOw==";
+
+const char* const TEST_USER2 = "user2";
+const char* const TEST_PWD2 = "RAgP1i7r+jLQxFd+VmL/5NBNznPtmW/6YxoTQ4QOZvNptzTbxbAz1zE5jUemdXKRYD9SK7tFlmwV45EdVbeYlD8Lt85Xr74b220z6klJFm2RjjZTr121xJeoUrUDV9FRhcTfhabbMndrBCIEo7NYTJ7K/9MoiimxbMgIUTHOVEqygNDKH7QxAW5BNCSYj8Utcilgd2Qeulx/sGMSfLcrdqdlAg2Y19f3h/QPRVNu5pGitagstapOnn6S+NCPV17WTGIfU1UKhL+n+60zSs/SxVNHrj/wbw+kO2HnN5RKGuJTHkPCGmN9aqX1i1EVm0KobQw4zJSo4w8VbPOyQhcCPQ==";
 
 TEST(AuthUnittest, Normal) {
   Auth auth;
-  auth.Authenticate(TEST_USER, TEST_PWD, [](ErrorPtr err, bool ok) {
+  auth.Authenticate(TEST_USER1, TEST_PWD1, [](ErrorPtr err, bool ok) {
     CHECK(err.get() == NULL);
     CHECK(ok);
   });
-  auth.Authenticate(TEST_USER, TEST_PWD, [](ErrorPtr err, bool ok) {
+  auth.Authenticate(TEST_USER1, TEST_PWD1, [](ErrorPtr err, bool ok) {
+    CHECK(err.get() == NULL);
+    CHECK(ok);
+  });
+  auth.Authenticate(TEST_USER2, TEST_PWD2, [](ErrorPtr err, bool ok) {
+    CHECK(err.get() == NULL);
+    CHECK(ok);
+  });
+  auth.Authenticate(TEST_USER2, TEST_PWD2, [](ErrorPtr err, bool ok) {
     CHECK(err.get() == NULL);
     CHECK(ok);
   });
