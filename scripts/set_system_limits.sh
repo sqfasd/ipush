a9f759be101f0a139aadc425bfa8ee504ebe335d
sqfasd <sqf1225@foxmail.com>
2015-04-21 15:27:07 +0800

    modify system limits setting script

--- scripts/set_system_limits.sh
+++ scripts/set_system_limits.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -x
+
 SYSCTL_CONF=/etc/sysctl.conf
 LIMITS_CONF=/etc/security/limits.conf
 
-cp $SYSCTL_CONF ${SYSCTL_CONF}.bak
+test -f ${SYSCTL_CONF}.bak || mv $SYSCTL_CONF ${SYSCTL_CONF}.bak
 cat >> /etc/sysctl.conf <<EOF
 net.core.somaxconn = 60000
 net.core.rmem_default = 4096
@@ -23,14 +25,14 @@ net.ipv4.tcp_tw_recycle = 1
 net.ipv4.tcp_max_orphans = 131072
 net.ipv4.tcp_keepalive_time = 1200
 net.ipv4.tcp_max_tw_buckets = 60000
-net.netfilter.nf_conntrack_max = 1000000
-net.netfilter.nf_conntrack_tcp_timeout_established = 1200
 fs.file-max = 2000000
 fs.nr_open = 1900000
 EOF
 
-cp $LIMITS_CONF ${LIMITS_CONF}.bak
+test -f ${LIMITS_CONF}.bak || mv $LIMITS_CONF ${LIMITS_CONF}.bak
 cat >> /etc/security/limits.conf <<EOF
 root    soft    nofile  1700000
 root    hard    nofile  1800000
 EOF
+
+sysctl -w
